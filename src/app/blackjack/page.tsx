2952a126d66bde1fa3bed82e77d0faacdd20f12b
Janus <janusstivang@gmail.com>
2026-02-23 14:02:05 +0100

    Add payouts & probability of busting

--- src/app/blackjack/page.tsx
+++ src/app/blackjack/page.tsx
@@ -13,7 +13,7 @@ export default function BlackJackPage() {
                 variant="h4"
                 sx={{ mb: 3, textAlign: "center", fontWeight: "bold" }}
             >
-                 Black Jack
+                Black Jack
             </Typography>
 
             {/* Knapper til at skifte mellem tabs */}
@@ -35,6 +35,22 @@ export default function BlackJackPage() {
             {/* Indhold */}
             {activeTab === "cheatsheet" && (
                 <Box sx={{ textAlign: "center" }}>
+                    <Typography variant="h6" sx={{ fontWeight: "bold", mb: 1 }}>
+                        Player hand vs busting
+                    </Typography>
+                    <ul style={{ listStyle: 'none', paddingLeft: 0 }}>
+                        <li><strong>Player Hand Total 21:</strong> Probability of Busting 100%</li>
+                        <li><strong>Player Hand Total 20:</strong> Probability of Busting 92%</li>
+                        <li><strong>Player Hand Total 19:</strong> Probability of Busting 85%</li>
+                        <li><strong>Player Hand Total 18:</strong> Probability of Busting 77%</li>
+                        <li><strong>Player Hand Total 17:</strong> Probability of Busting 69%</li>
+                        <li><strong>Player Hand Total 16:</strong> Probability of Busting 62%</li>
+                        <li><strong>Player Hand Total 15:</strong> Probability of Busting 58%</li>
+                        <li><strong>Player Hand Total 14:</strong> Probability of Busting 56%</li>
+                        <li><strong>Player Hand Total 13:</strong> Probability of Busting 39%</li>
+                        <li><strong>Player Hand Total 12:</strong> Probability of Busting 31%</li>
+                    </ul>
+
                     {/* SKIFT TIL BLACK JACK BILLEDE */}
                     <Image
                         src="/images/black-jack-cheatsheet.jpg"
@@ -43,7 +59,11 @@ export default function BlackJackPage() {
                         height={300}
                         style={{ width: "100%", height: "auto", margin: "0 auto" }}
                     />
+
+
                 </Box>
+
+
             )}
 
             {/* Player Options */}
@@ -53,14 +73,15 @@ export default function BlackJackPage() {
                         Player Actions
                     </Typography>
                     <ul>
-                        <li><strong>Hit:</strong> Træk et ekstra kort.</li>
-                        <li><strong>Stand:</strong> Behold nuværende hånd.</li>
-                        <li><strong>Double Down:</strong> Dobbler indsatsen, træk ét kort og stå.</li>
-                        <li><strong>Split:</strong> Del to ens kort i to hænder, matches på værdi kan godt være en 10'er og et billedekort.</li>
-                        <li><strong>Surrender:</strong> Opgiv hånden og mist halvdelen af indsatsen.</li>
-                        <li><strong>Insurance:</strong> Sideindsats mod dealerens BlackJack - Hvis det første dealer kort er et es.</li>
-                        <li><strong>Even Money:</strong> Tag 1:1 udbetaling hvis du har BlackJack mod dealerens es.</li>
-                        <li><strong>Push:</strong> Uafgjort, indsatsen returneres.</li>
+                        <li><strong>Hit:</strong> Træk et ekstra kort. Betaler 1:1</li>
+                        <li><strong>Stand:</strong> Behold nuværende hånd. Betaler 1:1</li>
+                        <li><strong>Double Down:</strong> Dobbler indsatsen, træk ét kort og stå. Betaler 1:1 pr hånd</li>
+                        <li><strong>Split:</strong> Del to kort med samme værdi i to hænder. Betaler 1:1 pr hånd</li>
+                        <li><strong>Surrender:</strong> Opgiv hånden og mist halvdelen af indsatsen. Betaler halvdelen tilbage</li>
+                        <li><strong>Insurance:</strong> kun når dealerens opkort er et es - Læg halvdelen af indsatsen. Hvis dealer har blackjack, betaler insurance 2:1 på insurance-bettet.</li>
+                        <li><strong>Five card charlie:</strong> Hvis der bliver lagt fem kort, uden at gå bust. Betaler 2:1</li>
+                        <li><strong>Blackjack:</strong> Hvis spillerens to første kort giver 21. Betaler 3:2 "bet 10, gevinst 15 total win = 25"</li>
+                        <li><strong>Push:</strong> Uafgjort - indsatsen returneres.</li>
                     </ul>
 
                     <Typography variant="h6" sx={{ fontWeight: "bold", mt: 3, mb: 1 }}>
@@ -71,7 +92,7 @@ export default function BlackJackPage() {
                         2. Spillerne kan "Hit" for flere kort eller "Stand" for at beholde deres hånd.<br />
                         3. Målet er at nå så tæt på 21 som muligt uden at gå over.<br />
                         4. Kort med billedværdi tæller som 10, Esser kan være 1 eller 11.<br />
-                        5. Dealer trækker til mindst 17.
+                        5. Dealer trækker til mindst 17-21, står på soft-17 (fx es + 6 = soft 17).
                     </Typography>
                 </Box>
             )}
